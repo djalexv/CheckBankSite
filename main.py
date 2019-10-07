e43e8e354721112f6a7e7e02392d7bf6acc1ba79
Alex <alex.4soft@mail.ru>
2019-10-07 17:01:37 +0700

    003

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import requests
 
 if __name__ == '__main__':
-    # print(__name__)
     path = 'tb.txt'
     with open(path) as f:
         for line in f:
-            print(line.strip())
-            # print(type(line))
-            r = requests.get(line).status_code
-            print(r)
-            # print(requests.get(line))
-            # .status_code)
+            try:
+                print(line.strip())
+                r = requests.get(line).status_code
+                print(r)
+                # if r != 200: сделать ping -проверить доступнсть
+            # except Exception: # разобрать
+            #     r = -1
+            except requests.ConnectionError:
+                print('something network trouble')
+            except requests.RequestException:
+                print('alarm, is disaster happen')
 
-    # r = requests.get('https://alfabank.ru/' )
-    # print(r.status_code)
