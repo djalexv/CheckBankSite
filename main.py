151a04117305d3f654c640eb8bf172a88a6273d6
Alex <alex.4soft@mail.ru>
2019-10-07 16:29:10 +0700

    002

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 import requests
 
-r = requests.get('https://alfabank.ru/' )
-print(r.status_code)
+if __name__ == '__main__':
+    # print(__name__)
+    path = 'tb.txt'
+    with open(path) as f:
+        for line in f:
+            print(line.strip())
+            # print(type(line))
+            r = requests.get(line).status_code
+            print(r)
+            # print(requests.get(line))
+            # .status_code)
+
+    # r = requests.get('https://alfabank.ru/' )
+    # print(r.status_code)
